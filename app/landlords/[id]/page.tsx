c772567924af1d5dd90cacec21d0cfa3ea1592bf
JaeguKang <renine94.dev@gmail.com>
2024-02-24 15:13:03 +0900

    feat: landlord 페이지 구현 (집주인)

--- app/landlords/[id]/page.tsx
+++ app/landlords/[id]/page.tsx
@@ -1,3 +1,5 @@
+import ContactButton from "@/app/components/ContactButton";
+import PropertyList from "@/app/components/properties/PropertyList";
 import Image from "next/image";
 
 const LandlordDetailPage = () => {
@@ -13,10 +15,16 @@ const LandlordDetailPage = () => {
               height={200}
               className="rounded-full"
             />
+            <h1 className="mt-6 text-2xl">Landlord name</h1>
+            <ContactButton />
           </div>
         </aside>
 
-        <div className="col-span-3 pl-0 md:pl-6">properties</div>
+        <div className="col-span-1 md:col-span-3 pl-0 md:pl-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <PropertyList />
+          </div>
+        </div>
       </div>
     </main>
   );
