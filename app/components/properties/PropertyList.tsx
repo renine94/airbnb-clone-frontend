66500068ed23cbc9f29e046f4c3e1eab3832f026
JaeguKang <renine94.dev@gmail.com>
2024-02-29 18:23:21 +0900

    feat: backend 에서 property data 가져오도록 변경

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,11 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
 
+export type PropertyType = {
+  id: string;
+  title: string;
+  price_per_night: number;
+  image_url: string;
+};
+
 const PropertyList = () => {
+  const [properties, SetProperties] = useState<PropertyType[]>([]);
+
+  const getProperties = async () => {
+    const url = "http://localhost:8000/api/v1/property/";
+
+    await fetch(url, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log("json", json);
+        SetProperties(json);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  };
+
+  useEffect(() => {
+    getProperties();
+  }, []);
+
   return (
     <>
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
+      {properties.map((property) => {
+        return <PropertyListItem key={property.id} property={property} />;
+      })}
     </>
   );
 };
